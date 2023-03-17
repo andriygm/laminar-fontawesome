2bddeb276313359a31eaa6d00fff599905aa9efe
Andriy Massimilla <andriymassimilla@gmail.com>
2023-03-17 12:23:22 -0400

    Initial commit

---
+++ FontAwesome.scala
@@ -0,0 +1,31 @@
+package org.ams.pipeline.components.atoms
+
+import scala.language.implicitConversions
+import scala.scalajs.js
+import scala.scalajs.js.`|`
+import com.raquo.laminar.api.L._
+import com.raquo.laminar.api.L.svg._
+import typings.fortawesomeFontawesomeCommonTypes.mod._
+import typings.fortawesomeFontawesomeSvgCore.{mod => fa}
+
+object FontAwesome {
+  def abstractElementToL(tree: fa.AbstractElement): SvgElement = {
+    import com.raquo.domtypes.generic.codecs._
+
+    customSvgTag(tree.tag)(
+      tree.attributes.asInstanceOf[js.Dictionary[String | Double]]
+        .filter({case (key, value) => key != "xmlns"})
+        .map {
+          case (key, value) => {
+            customSvgAttr(key, StringAsIsCodec) := value.toString
+          }
+        }.toSeq,
+      tree.children.fold(new js.Array[SvgElement])(_.map(abstractElementToL)),
+    )
+  }
+
+  implicit def renderFontAwesomeIcon(icon: IconLookup): SvgElement =
+    abstractElementToL(fa.icon(icon).`abstract`(0))
+
+  def apply(icon: IconLookup): SvgElement = renderFontAwesomeIcon(icon)
+}
